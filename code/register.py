c04338c47591d86976671bc308ea9475e2cbdd39
Junnie <ngocdung2805@gmail.com>
2024-04-14 21:55:14 -0400

    Add new dataloader Articles

--- code/register.py
+++ code/register.py
@@ -8,6 +8,8 @@ if world.dataset in ['gowalla', 'yelp2018', 'amazon-book']:
     dataset = dataloader.Loader(path="../data/"+world.dataset)
 elif world.dataset == 'lastfm':
     dataset = dataloader.LastFM()
+elif world.dataset == '12articles':
+    dataset = dataloader.Articles()
 
 print('===========config================')
 pprint(world.config)
